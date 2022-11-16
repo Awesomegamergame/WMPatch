eef2b580c087df33356a8386a189c023ce08b59f
awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2022-11-16 08:27:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WMPatch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipArchive archive = ZipFile.OpenRead("Test Patch.wmpatch");
+            var sample = archive.GetEntry("manifest.json");
+            Stream zipEntryStream = sample.Open();
+            StreamReader reader = new StreamReader(zipEntryStream);
+            string json = reader.ReadToEnd();
+            Console.WriteLine(json);
+            Console.WriteLine('\n');
+            WMManifest manifest = JsonConvert.DeserializeObject<WMManifest>(json);
+            Console.WriteLine("manifest" + manifest.manifest);
+            Console.WriteLine("name" + manifest.name);
+            Console.WriteLine("author" + manifest.author);
+            Console.WriteLine("version" + manifest.version);
+            Console.WriteLine("cover" + manifest.cover);
+            Console.WriteLine("userValue" + manifest.userValue);
+            Console.WriteLine("description" + manifest.description);
+            foreach (string file in manifest.patchFiles)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WMManifest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMPatch
+{
+    public class WMManifest
+    {
+        public string manifest { get; set; }
+        public string name { get; set; }
+        public string author { get; set; }
+        public string version { get; set; }
+        public string cover { get; set; }
+        public bool userValue { get; set; }
+        public string description { get; set; }
+        public IList<string> patchFiles { get; set; }
+        public WMManifest()
+        {
+
+        }
+    }
+}
